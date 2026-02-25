3af362982816b80a58140e43ea51ae367dc12947
lauuuu8 <119016085+lauuuu8@users.noreply.github.com>
2026-02-25 12:40:37 -0500

    Monedas

--- Proyecto/Assets/_Scripts/Platformer/PlayerMove.cs
+++ Proyecto/Assets/_Scripts/Platformer/PlayerMove.cs
@@ -4,22 +4,26 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
-    private OldInput _oldInput;
+    private NewInput _newInput;
+    private Rigidbody2D _rb;
     public float speed;
-
+    
     // Start is called before the first frame update
     void Start()
     {
-        _oldInput = GetComponent<OldInput>();
+        PlayerStats.score = 0;
+        _newInput = GetComponent<NewInput>();
+        _rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         Movement();  
     }
     public void Movement() {
 
-        transform.Translate(Vector3.right * _oldInput.horizontal * speed * Time.deltaTime);
+       // transform.Translate(Vector3.right * _newInput.inputX * speed * Time.deltaTime);
+       _rb.velocity = new Vector2(_newInput.inputX * speed, _rb.velocity.y);
     }
 }
